44f34f241e666a0997fe4bd2ae65d93d2e96ff2f
shenao <shenao@pcaingenieria.com>
2021-09-20 00:32:52 -0500

    agregacion del servicio de base de datos

---
+++ store/database_postgresql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE  if not exists fruits (id serial , name varchar(50));
+truncate table fruits;
+insert into fruits (id , name) values (1 ,'Apple') ;
+insert into fruits (id , name) values (2 ,'Orange') ;
+insert into fruits (id , name) values (3 ,'Pear') ;
+insert into fruits (id , name) values (4 ,'Strawberry'); 
--- store/fruits/api.py
+++ store/fruits/api.py
@@ -1,10 +1,36 @@
 from flask import Flask
-
+import psycopg2
+from psycopg2 import Error
 app =  Flask(__name__)
 
 @app.route('/')
 def fruits():
-	return { 'fruits':['Apple', 'Orange', 'Pear', 'Strawberry']}
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="unicorn_user",
+                                    password="magical_password",
+                                    host="0.0.0.0",
+                                    port="5432",
+                                    database="rainbow_database")
+
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Executing a SQL query
+        cursor.execute("SELECT * FROM fruits;")
+        # Fetch result
+        record = cursor.fetchall()
+        information = {"fruits":[]}
+        for fruit in record:
+            information["fruits"].append(fruit[1])
+        return information
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+	
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
