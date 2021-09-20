0c93d2ddc3b6c3e6eb263ac94b2c8ebaa015d1dc
shenao <shenao@pcaingenieria.com>
2021-09-20 02:27:28 -0500

    cambios finales

--- store/fruits/api.py
+++ store/fruits/api.py
@@ -1,32 +1,21 @@
 from flask import Flask
-import psycopg2
-from psycopg2 import Error
+from sqlalchemy import create_engine
 app =  Flask(__name__)
-
+db_name = 'rainbow_database'
+db_user = 'unicorn_user'
+db_pass = 'magical_password'
+db_host = 'db'
+db_port = '5432'
+# Connecto to the database
+db_string = 'postgres://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+db = create_engine(db_string)
 @app.route('/')
 def fruits():
-    try:
-        # Connect to an existing database
-        connection = psycopg2.connect(user="unicorn_user",
-                                    password="magical_password",
-                                    host="db",
-                                    port="5436",
-                                    database="rainbow_database")
-
-        # Create a cursor to perform database operations
-        cursor = connection.cursor()
-        # Executing a SQL query
-        cursor.execute("SELECT * FROM fruits;")
-        # Fetch result
-        record = cursor.fetchall()
-        information = {"fruits":[]}
-        for fruit in record:
-            information["fruits"].append(fruit[1])
-        return information
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-        return {"error":"connection failed"}
-	
+    querie = db.execute("SELECT * FROM fruits;")
+    print(querie)
+    print(type(querie))
+    return {"message":"hola mundo"}
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
