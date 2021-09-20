68a9dec0b377433dfdf1067976f0ed8277429142
Ubuntu <ubuntu@ip-172-31-28-169.ec2.internal>
2021-09-20 14:00:28 +0000

    cambios finales

--- store/fruits/api.py
+++ store/fruits/api.py
@@ -13,13 +13,13 @@ def fruits():
     try:
         db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
         db = create_engine(db_string)
-        querie = db.execute("SELECT * FROM fruits;").fetchall()
-        print(querie)
-        print(type(querie))
-        return {"message":"proceso realizado correctamente","type":type(querie),"querie":querie}
+        querie_fruits = db.execute("SELECT * FROM fruits;").fetchall()
+        information = {"fruits":[]}
+        for fruit in querie_fruits:
+            information["fruits"].append(fruit[1])
+        return information
     except:
-        return {"message":"error conectando a la base de datos"}
-    
+	return {"status":"error in database connection"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
