9808fc57dbe42bed303c56913ae379c70dd91c7a
shenao <shenao@pcaingenieria.com>
2021-09-20 02:51:11 -0500

    cambios finales

--- store/fruits/api.py
+++ store/fruits/api.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from sqlalchemy import create_engine
 app =  Flask(__name__)
-db_name = 'rainbow_database'
-db_user = 'unicorn_user'
-db_pass = 'magical_password'
-db_host = 'db'
-db_port = '5432'
-# Connecto to the database
 
-db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
-db = create_engine(db_string)
 @app.route('/')
 def fruits():
+    db_name = 'rainbow_database'
+    db_user = 'unicorn_user'
+    db_pass = 'magical_password'
+    db_host = 'db'
+    db_port = '5432'
+    # Connecto to the database
+
+    db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+    db = create_engine(db_string)
     querie = db.execute("SELECT * FROM fruits;")
     print(querie)
     print(type(querie))
